8c26b02ea54d623ac527324d0894e62909a2280c
jasser <labiadhjasser@protonmail.com>
2025-04-06 19:29:31 +0300

    fixed typo

--- hearbeatReceiver/src/main/java/dev/jasser/heartBeatReceiver/KafkaProducer.java
+++ hearbeatReceiver/src/main/java/dev/jasser/heartBeatReceiver/KafkaProducer.java
@@ -1,5 +1,6 @@
 package dev.jasser.heartBeatReceiver;
 
+import dev.jasser.heartBeatReceiver.proto.Heartbeat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -7,16 +8,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaProducer implements MqProducer {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, Heartbeat> kafkaTemplate;
 
     @Autowired
-    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+    public KafkaProducer(KafkaTemplate<String, Heartbeat> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     @Override
-    public void send(String topic, String message) {
+    public void send(String topic, Heartbeat heartbeat) {
         // Send the message to the given Kafka topic
-        kafkaTemplate.send(topic, message);
+        kafkaTemplate.send(topic, heartbeat);
     }
 }
