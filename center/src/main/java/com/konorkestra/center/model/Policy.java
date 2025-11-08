4c2f057f8eca4e5811cac48ffe4f40307744d803
jasser labiadh <jasser@jasser.dev>
2025-11-08 22:47:57 +0300

    defined "policy" for the mvp, to implemented policy parser and policyExecution classes

--- center/src/main/java/com/konorkestra/center/model/Policy.java
+++ center/src/main/java/com/konorkestra/center/model/Policy.java
@@ -1,29 +1,13 @@
 package com.konorkestra.center.model;
-import java.util.Map;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Policy {
-    private String uid;
+    private String uid;                // Unique identifier for the policy
+    private Integer minNodesRequired;  // Optional: used only for QUORUM type policies
     private String type;
-    private Map<String,Object> parameters;
-    
-    public Policy() {}
-    public Policy(String uid, String type, Map<String, Object> parameters) {
-        this.uid = uid;
-        this.type = type;
-        this.parameters = parameters;
-    }
-    public String getUid() {
-        return uid;
-    }
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-    public String getType() {
-        return type;
-    }
-    public void setType(String type) {
-        this.type = type;
-    }
-    public Map<String, Object> getParameters() {
-        return parameters;
-    }
+    private String description;
 }
